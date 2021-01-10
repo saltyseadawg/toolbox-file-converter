d43f1d4241fa3b051eb9e22e5e777abecdc57255
saltyseadawg <sabrinayu7@gmail.com>
2021-01-10 16:06:33 -0500

    parse sli file line by line

--- toolbox_file_converter/cli.py
+++ toolbox_file_converter/cli.py
@@ -1,15 +1,25 @@
 import click
 
+import text_utils
+
+DO_NOT_ALIGN_TAG = 'do-not-align'
 
 @click.command()
-@click.option("-f", "--filename", required=True)
+@click.option("-f", "--toolboxfile", required=True)
 @click.option("-o", "--output", required=True)
 @click.option("-t", "--tag", required=True)
+@click.option("-l", "--language", required=True)
 def convert(filename, output, tag):
     """
     Converts in_file into an xml out_file for Readalongs Studio.
     Exclude_tags are converted into xml objects that won't be aligned.
     """
-
+    root = ET.Element('TEI')
     # Assuming that only one tag will be aligned
-    pass
+    with open(filename) as fp:
+
+        lines = fp.readlines()
+        for line in lines:
+            if not text_utils.get_toolbox_tag(line)
+                continue
+            
