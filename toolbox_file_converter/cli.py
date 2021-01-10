9e34af77aa3ee7eed59dbed327aab7a410f6009c
saltyseadawg <sabrinayu7@gmail.com>
2021-01-10 17:01:35 -0500

    setup base xml nodes

--- toolbox_file_converter/cli.py
+++ toolbox_file_converter/cli.py
@@ -8,13 +8,25 @@ DO_NOT_ALIGN_TAG = 'do-not-align'
 @click.option("-f", "--toolboxfile", required=True)
 @click.option("-o", "--output", required=True)
 @click.option("-t", "--tag", required=True)
-@click.option("-l", "--language", required=True)
-def convert(filename, output, tag):
+@click.option("-l", "--lang", required=True)
+def convert(filename, output, tag, lang):
     """
     Converts in_file into an xml out_file for Readalongs Studio.
     Exclude_tags are converted into xml objects that won't be aligned.
     """
+    readalongs_attr = {
+        'xml:lang': lang
+    }
+
+    div_attr = {
+        'type': 'page'
+    }
+
     root = ET.Element('TEI')
+    text = ET.SubElement(parent=root, tag='text', attrib=readalongs_attr)
+    body = ET.SubElement(parent=text, tag='body')
+    div = ET.SubElement(parent=body, tag='div', attrib=div_attr)
+
     # Assuming that only one tag will be aligned
     with open(filename) as fp:
 
