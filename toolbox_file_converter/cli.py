7e74b7cd187a716ef939656dcc9ca4377b3f18f3
saltyseadawg <sabrinayu7@gmail.com>
2021-01-16 16:46:38 -0500

    add remove_tag function to remove tag from toolbox lines - change project max line length to 90 - add setup.cfg file - lint files

--- toolbox_file_converter/cli.py
+++ toolbox_file_converter/cli.py
@@ -1,4 +1,4 @@
-import click
+# import click
 from lxml import etree as ET
 
 from toolbox_file_converter import toolbox_utils
@@ -17,35 +17,28 @@ def convert(filename, output, tag, lang):
 
     # attr_qname = ET.QName(lang, "lang")
     # namespace_prefix = "{%s}" % lang
-    NSMAP = {
-        'lang': lang
-    }
+    NSMAP = {"lang": lang}
 
-    div_attr = {
-        'type': 'page'
-    }
+    div_attr = {"type": "page"}
 
-    no_align_attr = {
-        'do-not-align': "true"
-    }
+    no_align_attr = {"do-not-align": "true"}
 
-    root = ET.Element('TEI')
+    root = ET.Element("TEI")
     tree = ET.ElementTree(root)
-    text = ET.SubElement(root, 'text', nsmap=NSMAP)
-    body = ET.SubElement(text, 'body')
-    
+    text = ET.SubElement(root, "text", nsmap=NSMAP)
+    body = ET.SubElement(text, "body")
 
     # Assuming that only one tag will be aligned
     with open(filename) as fp:
         lines = fp.readlines()
-        page = ET.SubElement(body, 'div', div_attr)
-        p_element = ET.SubElement(page, 'p')
+        page = ET.SubElement(body, "div", div_attr)
+        p_element = ET.SubElement(page, "p")
         for line in lines:
             if toolbox_utils.get_toolbox_tag(line) == tag:
-                page = ET.SubElement(body, 'div', div_attr)
-                p_element = ET.SubElement(page, 'p')
-                ET.SubElement(p_element, 's').text = line
-            else: 
-                ET.SubElement(p_element, 's', no_align_attr).text = line
-    
-    tree.write(output, xml_declaration=True, encoding='UTF-8', pretty_print=True)
+                page = ET.SubElement(body, "div", div_attr)
+                p_element = ET.SubElement(page, "p")
+                ET.SubElement(p_element, "s").text = line
+            else:
+                ET.SubElement(p_element, "s", no_align_attr).text = line
+
+    tree.write(output, xml_declaration=True, encoding="UTF-8", pretty_print=True)
